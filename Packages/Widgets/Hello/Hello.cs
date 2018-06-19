73d13d821e776d5c736a02700e95f329a28fdb82
jakubsenk <jakub.senk@keenmate.com>
2018-06-19 17:11:13 +0200

    Sample hello widget

---
+++ Packages/Widgets/Hello/Hello.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogifier.Widgets
+{
+	[ViewComponent(Name = "Hello")]
+	public class Hello : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ Packages/Widgets/Hello/PackageInfo.cs
@@ -0,0 +1,19 @@
+﻿using Blogifier.Core.Data.Models;
+using System.Reflection;
+
+public class PackageInfo : IPackageInfo
+{
+	public PackageListItem GetAttributes()
+	{
+		return new PackageListItem
+		{
+			Title = "Hello",
+			Version = "1.0.0",
+			Description = "Hello widget.",
+			Icon = "https://avatars0.githubusercontent.com/u/19671571?v=4&amp;s=180",
+			Author = "Tester",
+			ProjectUrl = "https://github.com/blogifierdotnet/Blogifier",
+			Tags = "widget,hello,test"
+		};
+	}
+}
